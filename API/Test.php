d08753df4369cd5cccd293fdf450d1351a94805b
Alex Ibeh <harlexibeh01@gmail.com>
2021-06-13 09:29:37 +0100

    Update

--- API/Test.php
+++ API/Test.php
@@ -2,16 +2,10 @@
      header("Access-Control-Allow-Origin: *");
      session_start();
 
-     if(isset($_POST['busid']) || isset($_POST['userid']))
-     {
+  
         $BusName = $_POST['busid'];
         $UserID = $_POST['userid'];
 
         echo 'Test::Data Received :: BusName=> ' . $BusName . ' UserID=> ' . $UserID;
-     }
-
-     else 
-     {
-          echo 'Response::Error';
-     }
+  
 ?>
