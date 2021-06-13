e85854981a7a07aebb9629aa5f35c9ceb55216a4
Alex Ibeh <harlexibeh01@gmail.com>
2021-06-13 10:10:21 +0100

    UPdate

--- API/Test.php
+++ API/Test.php
@@ -3,9 +3,9 @@
      //session_start();
      header("Content-Type: application/json; charset=UTF-8");
   
-        $BusName = $_POST['busid'];
-        $UserID = $_POST['userid'];
+        $BusName = $_GET['busid'];
+        $UserID = $_GET['userid'];
 
-        echo 'Test::Data Received :: BusName=> ' . $_POST['busid'] . ' UserID=> ' . $_POST['userid'];
+        echo 'Test::Data Received :: BusName=> ' . $_GET['busid'] . ' UserID=> ' . $_GET['userid'];
   
 ?>
