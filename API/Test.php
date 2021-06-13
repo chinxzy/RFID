efa92180d27ad5143dcf2231e67f49a2c7ce5cca
Alex Ibeh <harlexibeh01@gmail.com>
2021-06-13 09:11:47 +0100

    Update

---
+++ API/Test.php
@@ -0,0 +1,19 @@
+<?php
+     header("Access-Control-Allow-Origin: *");
+     session_start();
+
+     if(!isset($_GET['busid']) || !isset($_GET['userid']))
+     {
+        $BusName = $_GET['busid'];
+        $UserID = $_GET['userid'];
+
+        echo 'Test::Data Received :: BusName=> ' . $BusName . ' UserID=> ' . $UserID;
+     }
+
+     else 
+     {
+          echo 'Response::Error'
+     }
+}
+
+?>
