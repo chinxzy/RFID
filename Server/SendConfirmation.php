6851e0c45d2d5987a20b6e13db0c39ef77a5016d
Alex Ibeh <harlexibeh01@gmail.com>
2021-07-20 11:26:10 +0100

    u

--- Server/SendConfirmation.php
+++ Server/SendConfirmation.php
@@ -11,7 +11,7 @@
 	$Email= $_GET['Email'];
 	$ConfirmationKey= $_GET['Key'];
 
-	$Sql="SELECT * FROM EmailConfirmation WHERE Email= '$Email'";
+	/*$Sql="SELECT * FROM EmailConfirmation WHERE Email= '$Email'";
 	$Result=$Connection->query($Sql);
     $mail = new PHPMailer;
     $mail->isSMTP();
@@ -35,5 +35,32 @@
    {
 	   $Connection->query($Sql);
 	   header("Location: ../Client/Admin/MailSent.html");
-   }
+   }*/
+
+   $curl = curl_init();
+    curl_setopt_array($curl, array(
+    CURLOPT_URL => $_ENV['TRUSTIFI_URL'] . "/api/i/v1/email",
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => "",
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => "POST",
+    CURLOPT_POSTFIELDS =>"{\"recipients\":[{\"email\":\"test@trustificorp.org\"}],\"title\":\"Title\",\"html\":\"Body\"}",
+    CURLOPT_HTTPHEADER => array(
+        "x-trustifi-key: " . $_ENV['TRUSTIFI_KEY'],
+        "x-trustifi-secret: " . $_ENV['TRUSTIFI_SECRET'],
+        "content-type: application/json"
+    )
+   ));
+
+    $response = curl_exec($curl);
+    $err = curl_error($curl);
+    curl_close($curl);
+    if ($err) {
+    echo "cURL Error #:" . $err;
+    } else {
+    echo $response;
+    }
 ?>
